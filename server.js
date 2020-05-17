3baff1c788c15aa6e90e7e9008e0cb05be6e1702
Lloyd <lloydjvrensburg@gmail.com>
2020-05-17 23:59:00 +0200

    Final Commit Encrypto Verison 2

--- server.js
+++ server.js
@@ -35,6 +35,14 @@ app.post("/encrypt", (req, res) => {
   });
 });
 
+// Serve static assests in production
+if (process.env.NODE_ENV === "production") {
+  app.upp(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index,html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server running on port ${port}`));
