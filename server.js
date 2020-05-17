6906bb293e3b08c6ea991ab6e70762c7a94bea82
Lloyd <lloydjvrensburg@gmail.com>
2020-05-18 00:11:01 +0200

    Encrypto v2 commit

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post("/encrypt", (req, res) => {
 
 // Serve static assests in production
 if (process.env.NODE_ENV === "production") {
-  app.upp(express.static("client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index,html"));
   });
